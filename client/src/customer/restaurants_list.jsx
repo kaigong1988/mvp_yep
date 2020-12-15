2788956ef684f627ce118977094a92bfb492aec5
Kai Gong <kaigong@Kais-MacBook-Pro.local>
2020-12-15 16:22:46 -0500

    issue fix

--- client/src/customer/restaurants_list.jsx
+++ client/src/customer/restaurants_list.jsx
@@ -1,1 +1,3 @@
-const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.7605904,-73.8239152&destinations=40.6905615,-73.9976592&key=AIzaSyAEovQE9S6vpxQQnEzPzCxu3WrIGvkeIXA`;
+const keys = require('../../../config.js');
+
+const url = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.7605904,-73.8239152&destinations=40.6905615,-73.9976592&key=${keys.geoAPI}`;
