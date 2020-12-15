15d79f0e91108499d9f3f4371bd431850e65d2c9
Kai Gong <kaigong@Kais-MacBook-Pro.local>
2020-12-15 00:21:43 -0500

    set up basic structure

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      someState: "value of state"
+      someState: 'value of state',
+      currentZip: 11354,
+      currentCoords: {
+        lat: 0,
+        long: 0,
+      },
+      currentUser: 'default',
     };
   }
 
   render() {
-    return <div>Hello! Does this work?</div>;
+    if (this.state.currentUser === 'customer') {
+      return <div>I am an eater!</div>;
+    } else if (this.state.currentUser === 'owner') {
+      return <div>I am an owner!</div>;
+    } else {
+      return (
+        <div>
+          <input />
+          <button>Use my current location</button>
+        </div>
+      );
+    }
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
